7dad157361381fb8e2cc42e43cadd11513a36e60
Desdevises Simon <simon.desdevises@gmail.com>
2024-09-24 15:36:44 +0200

    feat : start c

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ pnpm install
 pnpm run dev:python S5/algo/TP1
 # sh script/start.python.sh {{path/to/file/or/folder}}
 ```
+
+### C
+
+```bash
+pnpm run dev:c S5/algo/TP1/test.c
+# sh script/start.c.sh {{path/to/file}}
+```
+
+This will compile the file and run it.
+The file will be compiled in the `bin` folder.
---
+++ script/start.c.sh
@@ -0,0 +1,16 @@
+path=$1
+filename=$(basename "$path")
+filename_without_ext=$(basename "$path" .c)
+directory=$(dirname "$path")
+
+bin_dir="$directory/bin"
+mkdir -p "$bin_dir"
+
+if [ "$filename" != "" ]; then
+  # compile c
+  gcc "$path" -o "$bin_dir/$filename_without_ext"
+  echo "build finish"
+  ./"$bin_dir/$filename_without_ext"
+else
+  echo "No path provided"
+fi
