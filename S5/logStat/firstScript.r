f03118d26fea56c67249f671b9e26e4c8574cd19
PolySim <simon.desdevises@gmail.com>
2024-10-21 10:22:58 +0200

    update script

--- S5/logStat/firstScript.r
+++ S5/logStat/firstScript.r
@@ -1,7 +1,21 @@
+<<<<<<< Updated upstream
 # library(dplyr)
+=======
+library(dplyr)
+>>>>>>> Stashed changes
 
-commune <- read.csv("communes.csv", sep = ";")
-immeubles <- read.csv("immeubles.csv", sep = ";")
+#commune <- read.csv("communes.csv", sep = ";")
+#immeuble <- read.csv("immeubles.csv", sep = ";")
 
+<<<<<<< Updated upstream
 merge(commune, immeubles, by="INSEE")
 # commune %>% select('INSEE')
+=======
+fusion <- inner_join(communes, immeubles, by="INSEE")
+
+library(ggplot2)
+
+data("diamonds")
+
+plot(diamonds$clarity, col="#A23456")
+>>>>>>> Stashed changes
