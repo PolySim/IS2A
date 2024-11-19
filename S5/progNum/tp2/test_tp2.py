b47b7aaee16966967bc5763c834ff9adbb37e92d
Desdevises Simon <simon.desdevises@gmail.com>
2024-11-19 13:42:04 +0100

    part 1

--- S5/progNum/tp2/test_tp2.py
+++ S5/progNum/tp2/test_tp2.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 # Cette commande importe la fonction à tester (à adapter !)
-from tp2 import eval_naive
+from tp2 import eval_naive, eval_Horner, eval_derivee
 
 # On écrit une fonction Python par test unitaire
 # Le contenu du test doit bien sûr être adapté au cas par cas
@@ -21,9 +21,45 @@ def test_eval_naive_3():
     A = np.array ([-1, 0, 1], dtype=np.float64)
     assert (eval_naive (1, A) == 0), "0 attendu"
 
+def test_eval_Horner_1():
+    A = np.array ([], dtype=np.float64)
+    assert (eval_Horner (17, A) == 0), "0 attendu"
+
+def test_eval_Horner_2():
+    A = np.array ([-1, 0, 1], dtype=np.float64)
+    assert (eval_Horner (-1, A) == 0), "0 attendu"
+
+def test_eval_Horner_3():
+    A = np.array ([-1, 0, 1], dtype=np.float64)
+    assert (eval_Horner (1, A) == 0), "0 attendu"
+
+def test_eval_Derivee_1():
+    A = np.array ([], dtype=np.float64)
+    assert (eval_derivee (17, A) == 0), "0 attendu"
+
+def test_eval_Derivee_2():
+    A = np.array ([-1, 0, 1], dtype=np.float64)
+    print(eval_derivee (0, A))
+    # assert (eval_derivee (0, A) == 0), "0 attendu"
+
+def test_eval_Derivee_3():
+    A = np.array ([-1, 0, 1], dtype=np.float64)
+    print(eval_derivee(1, A))
+    # assert (eval_derivee (1, A) == 2), "2 attendu"
+
 # La liste L des fonctions définies ci-dessus (à adapter !)
 
-L = [test_eval_naive_1, test_eval_naive_2, test_eval_naive_3]
+L = [
+    test_eval_naive_1,
+    test_eval_naive_2,
+    test_eval_naive_3,
+    test_eval_Horner_1,
+    test_eval_Horner_2,
+    test_eval_Horner_3,
+    test_eval_Derivee_1,
+    test_eval_Derivee_2,
+    test_eval_Derivee_3
+]
 
 # A partir d'ici, il n'y a plus rien à changer.
 # La boucle suivante exécute chaque test et compte les succès.
