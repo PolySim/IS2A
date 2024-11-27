0e1f602a092076735666d9566dbae4984f0841ec
Desdevises Simon <simon.desdevises@gmail.com>
2024-11-27 13:46:10 +0100

    init project

--- S5/bdd/TP4/index.php
+++ S5/bdd/TP4/index.php
@@ -2,8 +2,14 @@
     <html>
     <head>
         <title>TP4</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
     </head>
     <body>
+    <?php
+    require('utils.php');
+    $db = connect_to_database();
+    ?>
+
     <h1>TP4</h1>
     <p>Hello world</p>
     </body>
---
+++ S5/bdd/TP4/utils.php
@@ -0,0 +1,10 @@
+<?php
+function connect_to_database()
+{
+    $servername = "172.26.16.4";
+    $username = "sdesdevi";
+    $password = "postgres";
+    $databasename = "sdvideoclub";
+}
+
+?>
