ca15b54c073ee9787f840a22452cf64ab1a02af2
Desdevises Simon <simon.desdevises@gmail.com>
2024-12-01 12:05:03 +0100

    tp with dico

--- S5/algo/TP8/exercice.c
+++ S5/algo/TP8/exercice.c
@@ -33,10 +33,12 @@ Annuaire initAnnuaire(char *fileName) {
   }
 
   fscanf(file, "%s %s %s %d %d %d", person.lastName, person.firstName, person.mobile, &person.birthDate.day, &person.birthDate.month, &person.birthDate.year);
+  annuaire.lastPerson++;
+  annuaire.persons[annuaire.lastPerson] = person;
   while(!feof(file) && annuaire.lastPerson < MAX_PERSONS) {
+    fscanf(file, "%s %s %s %d %d %d", person.lastName, person.firstName, person.mobile, &person.birthDate.day, &person.birthDate.month, &person.birthDate.year);
     annuaire.lastPerson++;
     annuaire.persons[annuaire.lastPerson] = person;
-    fscanf(file, "%s %s %s %d %d %d", person.lastName, person.firstName, person.mobile, &person.birthDate.day, &person.birthDate.month, &person.birthDate.year);
   }
 
   fclose(file);
@@ -70,19 +72,6 @@ int compareStrings(char *str1, char *str2) {
   return res;
 }
 
-// To dico
-Person findPerson(Annuaire annuaire, char *mobile) {
-  Person person;
-  int i = 0;
-  printf("Recherche de la personne avec le mobile : %s\n", mobile);
-  while(person.mobile[0] == '\0' && i < MAX_PERSONS) {
-    if (compareStrings(annuaire.persons[i].mobile, mobile)) {
-      person = annuaire.persons[i];
-    }
-    i++;
-  }
-  return person;
-}
 
 Person initPerson(char *lastName, char *firstName, char *mobile, int day, int month, int year) {
   Person person;
@@ -95,30 +84,73 @@ Person initPerson(char *lastName, char *firstName, char *mobile, int day, int mo
   return person;
 }
 
-// Sorted
+int findIndexPerson(Annuaire annuaire, char *mobile) {
+  int start = 0, end = annuaire.lastPerson, currentIndex = annuaire.lastPerson / 2;
+  while (start <= end && strcmp(annuaire.persons[currentIndex].mobile, mobile)) {
+    if (atoi(annuaire.persons[currentIndex].mobile) > atoi(mobile)) {
+      end = currentIndex - 1;
+    } else {
+      start = currentIndex + 1;
+    }
+    currentIndex = (start + end) / 2;
+  }
+  if (start > end) {
+    return -1;
+  }
+  return currentIndex;
+}
+
+Person findPerson(Annuaire annuaire, char *mobile) {
+  int index = findIndexPerson(annuaire, mobile);
+  printf("Recherche de la personne avec le mobile : %s\n", mobile);
+  if (index == -1) {
+    return initPerson("", "", "", 0, 0, 0);
+  }
+  return annuaire.persons[index];
+}
+
+int findIndexToAddPerson(Annuaire annuaire, char *mobile) {
+  int start = 0, end = annuaire.lastPerson, currentIndex = annuaire.lastPerson / 2;
+  while (start <= end && !(strcmp(annuaire.persons[currentIndex].mobile, mobile) == -1 && strcmp(annuaire.persons[currentIndex + 1].mobile, mobile) >= 0) ) {
+    if (atoi(annuaire.persons[currentIndex].mobile) > atoi(mobile)) {
+      end = currentIndex - 1;
+    } else {
+      start = currentIndex + 1;
+    }
+    currentIndex = (start + end) / 2;
+  }
+  if (currentIndex == annuaire.lastPerson) {
+    return annuaire.lastPerson + 1;
+  }
+  if (start > end) {
+    return 0;
+  }
+  return currentIndex + 1;
+}
+
 void addPerson(Annuaire *annuaire, Person person) {
+  int i, index = findIndexToAddPerson(*annuaire, person.mobile);
   if ((*annuaire).lastPerson > MAX_PERSONS) {
     printf("Annuaire plein\n");
     return;
   }
-  (*annuaire).persons[(*annuaire).lastPerson + 1] = person;
+  for (i = (*annuaire).lastPerson + 1; i > index; i--) {
+    (*annuaire).persons[i] = (*annuaire).persons[i - 1];
+  }
+  (*annuaire).persons[index] = person;
   (*annuaire).lastPerson++;
 }
 
 void deletePerson(Annuaire *annuaire, char *mobile) {
-  int i = 0, personFound = 0;
-  while(i <= (*annuaire).lastPerson ) {
-    if (!personFound && compareStrings((*annuaire).persons[i].mobile, mobile)) {
-      personFound = 1;
-    }
-    if (personFound && i+1 < (*annuaire).lastPerson) {
-      (*annuaire).persons[i] = (*annuaire).persons[i + 1];
-    }
-    i++;
+  int i = 0;
+  int personIndex = findIndexPerson(*annuaire, mobile);
+  if (personIndex == -1) {
+    return;
   }
-  if (personFound) {
-    (*annuaire).lastPerson--;
+  for (i = personIndex; i < (*annuaire).lastPerson; i++) {
+    (*annuaire).persons[i] = (*annuaire).persons[i + 1];
   }
+  (*annuaire).lastPerson--;
 }
 
 void saveAnnuaire(Annuaire annuaire, char *fileName) {
@@ -143,26 +175,27 @@ int main(int argc, char *argv[]) {
   annuaire = initAnnuaire(argv[1]);
 
   printAnnuaire(annuaire);
+  printf("\n");
 
 //  printPerson(findPerson(annuaire, "0769389805"));
 //  printPerson(findPerson(annuaire, "0269389805"));
 //  printPerson(findPerson(annuaire, "02693d89805"));
 
-//  person = initPerson("Lavielle", "Zoe", "0102030405", 05, 05, 2004);
-//  printf("\n");
-//  printf("Ajout de la personne : ");
-//  printPerson(person);
-//  addPerson(&annuaire, person);
-//  printf("\n");
-//  printAnnuaire(annuaire);
-//  printf("\n");
-
+  person = initPerson("Lavielle", "Zoe", "0302030405", 05, 05, 2004);
+  printf("\n");
+  printf("Ajout de la personne : ");
+  printPerson(person);
+  addPerson(&annuaire, person);
+  printf("\n");
+  printAnnuaire(annuaire);
+  printf("\n");
+//
 //  printf("Suppression de la personne avec le mobile : %s\n \n", person.mobile);
 //  deletePerson(&annuaire, person.mobile);
 //
 //  printAnnuaire(annuaire);
 
-//  saveAnnuaire(annuaire, argv[2]);
+  saveAnnuaire(annuaire, argv[2]);
 
   return 0;
 }
