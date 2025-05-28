f94e0b5cade428c8c34e12bbaf76cfc53a67628d
Simon Desdevises <simon.desdevises@gmail.com>
2025-05-28 09:09:11 +0200

    ALN : Houselder matrice carre

--- S6/ALN/TP1/houselder.py
+++ S6/ALN/TP1/houselder.py
@@ -1,6 +1,6 @@
 import math
 
-A= \
+A_Test= \
 [
     [2, 1, 0, 4],
     [-2, -2, 3, -5],
@@ -10,7 +10,7 @@ A= \
     [4, -5, -3, -2]
 ]
 
-b = \
+b_Test = \
 [
     [2],
     [-9],
@@ -18,6 +18,22 @@ b = \
     [2],
     [-1],
     [0.5]
+]
+
+A= \
+[
+    [2, 1, 0, 4],
+    [-4, -2, 3, -5],
+    [4, 1, -2, 3],
+    [0, -3, -12, -1],
+]
+
+b = \
+[
+    [2],
+    [-9],
+    [2],
+    [2],
 ]
 
 def print_matrix(M: list[list[float]]):
@@ -61,9 +77,12 @@ def b_moins(b: list[list[float]], v: list[list[float]]) -> list[list[float]]:
 def get_column(M: list[list[float]], column: int) -> list[list[float]]:
     return [[M[i][column]] for i in range(len(M))]
 
-def replace_column(M: list[list[float]], column: int, new_column: list[list[float]]) -> list[list[float]]:
-    for i in range(len(M)):
-        M[i][column] = new_column[i][0]
+def replace_column(M: list[list[float]], column: int, new_column: list[list[float]], start: int = 0, with_0: bool = False) -> list[list[float]]:
+    for i in range(start, len(M)):
+        if (with_0 and i != start):
+            M[i][column] = 0
+        else:
+            M[i][column] = new_column[i - start][0]
     return M
 
 def remove_first_column(M: list[list[float]]) -> list[list[float]]:
@@ -72,59 +91,95 @@ def remove_first_column(M: list[list[float]]) -> list[list[float]]:
 def remove_first_row(M: list[list[float]]) -> list[list[float]]:
     return M[1:]
 
+def remonte(M: list[list[float]], b: list[list[float]]) -> list[list[float]]:
+    res = []
+    size_b = len(M[0])
+    for i in range(size_b - 1, -1, -1):
+        for j in range(size_b - 1, i - 1, -1):
+            if (j == size_b - 1):
+                if (i == size_b - 1):
+                    res = [[b[j][0] / M[i][j]]] + res
+                else:
+                    res = [[M[i][j] * res[-1][0]]] + res
+            else:
+                if (j == i):
+                    res[0][0] = (b[j][0] - res[0][0]) / M[i][j]
+                else:
+                    delta = size_b - j
+                    res[0][0] += M[i][j] * res[-1 * delta][0]
+
+    return res
+        
 def algorithme(A: list[list[float]], b: list[list[float]]):
+    # Clone matrice
     new_A = A[:]
     new_b = b[:]
+
     for round in range(len(A[0])):
+
+        # Calcule les données identique 
         norme_A = norme_first_column(new_A)
         v = calcule_v(new_A, norme_A)
         vt = transpose(v)
         vt_v_2 = produit_vt_v(new_A, norme_A)
         
         for i in range(len(new_A[0])):
+            # Pour chaque colonne de la sous matrice
             column = get_column(new_A, i)
             vt_b = produit_vt_b(vt[0], column)
             new_column = b_moins(column, produit_const_v(vt_b / vt_v_2, v))
+            # Met à jour la matrice A et new_A
             new_A = replace_column(new_A[:], i, new_column)
+            A = replace_column(A[:], i + round, new_column, start=round, with_0=i == 0)
 
+        # Met à jour la matrice b et new_b
         vt_b = produit_vt_b(vt[0], new_b)
         new_b = b_moins(new_b, produit_const_v(vt_b / vt_v_2, v))
         b = b[:round] + new_b[:]
+
+        # Supprime la première ligne et la première colonne de la matrice new_A et new_b pour la prochaine itération
         new_b = remove_first_row(new_b)
         new_A = remove_first_row(new_A)
         new_A = remove_first_column(new_A)
 
+    # Affiche les matrices A et b
+    print("\nMatrice A\n")
+    print_matrix(A)
+    print("\nMatrice b\n")
     print_matrix(b)
-    return new_A
+
+    print("\nRésultat\n")
+    res = remonte(A, b)
+    print_matrix(res)
+    return res
 
 
 
 
 if __name__ == "__main__":
-    assert get_column(A, 0) == [[2], [-2], [6], [2], [0], [4]]
-    assert get_column(A, 1) == [[1], [-2], [1], [-3], [1], [-5]]
-    assert get_column(A, 2) == [[0], [3], [-2], [-12], [6], [-3]]
-    assert get_column(A, 3) == [[4], [-5], [3], [-1], [0], [-2]]
+    assert get_column(A_Test, 0) == [[2], [-2], [6], [2], [0], [4]]
+    assert get_column(A_Test, 1) == [[1], [-2], [1], [-3], [1], [-5]]
+    assert get_column(A_Test, 2) == [[0], [3], [-2], [-12], [6], [-3]]
+    assert get_column(A_Test, 3) == [[4], [-5], [3], [-1], [0], [-2]]
 
-    assert replace_column(A[:], 0, get_column(A, 0)) == [[2, 1, 0, 4], [-2, -2, 3, -5], [6, 1, -2, 3], [2, -3, -12, -1], [0, 1, 6, 0], [4, -5, -3, -2]]
-    # assert replace_column(A[:], 0, get_column(A, 1)) == [[1, 1, 0, 4], [-2, -2, 3, -5], [1, 1, -2, 3], [-3, -3, -12, -1], [1, 1, 6, 0], [-5, -5, -3, -2]]
+    assert replace_column(A_Test[:], 0, get_column(A_Test, 0)) == [[2, 1, 0, 4], [-2, -2, 3, -5], [6, 1, -2, 3], [2, -3, -12, -1], [0, 1, 6, 0], [4, -5, -3, -2]]
 
-    assert remove_first_column(A[:]) == [[1, 0, 4], [-2, 3, -5], [1, -2, 3], [-3, -12, -1], [1, 6, 0], [-5, -3, -2]]
-
-    norme_A = norme_first_column(A)
+    norme_A = norme_first_column(A_Test)
     assert norme_A == 8
 
-    v = calcule_v(A, norme_first_column(A))
+    v = calcule_v(A_Test, norme_first_column(A_Test))
     vt = transpose(v)
     assert v == [[10], [-2], [6], [2], [0], [4]]
     assert vt == [[10, -2, 6, 2, 0, 4]]
     
-    vt_v_2 = produit_vt_v(A, norme_A)
+    vt_v_2 = produit_vt_v(A_Test, norme_A)
     assert vt_v_2 == 80
     
-    vt_b = produit_vt_b(vt[0], b)
+    vt_b = produit_vt_b(vt[0], b_Test)
     assert vt_b == 56
     
     # print_matrix(produit_const_v(vt_b / vt_v_2, v))
     # print_matrix(b_moins(b, produit_const_v(vt_b / vt_v_2, v)))
-    algorithme(A, b)
+    # algorithme(A, b)
+
+    # algorithme(A_Test, b_Test)
