6309d12e9d75dea39719a76007786f9cb8653e54
Simon Desdevises <simon.desdevises@gmail.com>
2025-06-03 17:43:22 +0200

    ALN : use numpy

--- S6/ALN/TP1/cholesky.py
+++ S6/ALN/TP1/cholesky.py
@@ -1,83 +1,79 @@
 from houselder import print_matrix, transpose, remonte, residu
-import math
+import numpy as np
 
-C = \
-[
+C = np.array([
     [1.0, 0.0, 1.0],
     [0.5, -1.0, 0.0],
     [0.0, 3.0, -5.0],
     [1.0, 0.0, -4.0]
-]
+], dtype=np.float64)
 
-b = \
-[
+b = np.array([
     [1.0],
     [-1.0],
     [4.0]
-]
+], dtype=np.float64)
 
-def init_matrix_carree(n: int) -> list[list[float]]:
-    return [[0.0 for _ in range(n)] for _ in range(n)]
 
-def produit_matrix_transpose(M_T: list[list[float]], M: list[list[float]]) -> list[list[float]]:
-    m = len(M)
-    n = len(M_T)
-    A = init_matrix_carree(n)
+def produit_matrix_transpose(M_T: np.ndarray, M: np.ndarray) -> np.ndarray:
+    A = np.zeros((M.shape[1], M.shape[1]), dtype=np.float64)
+    m = M.shape[0]
+    n = M.shape[1]
 
     for i in range(n):
-        # triangle supérieur
         for j in range(i, n):
-            somme = 0.0
-            # produit scalaire des colonnes i et j
-            for k in range(m):
-                somme += C[k][i] * C[k][j]
-            A[i][j] = somme
+            # Peut être inversé
+            somme = np.sum(M[:m, i] * M[:m, j])
+            A[i, j] = somme
             if i != j:
-                # symétrie
-                A[j][i] = somme
-
+                A[j, i] = somme
     return A
 
-def calcul_L(A: list[list[float]]) -> list[list[float]]:
-    n = len(A)
-    L = init_matrix_carree(n)
+def calcul_L(A: np.ndarray) -> np.ndarray:
+    n = A.shape[0]
+    L = np.zeros((n, n))
+    
     for row in range(n):
         for col in range(row + 1):
             # diagonale
             if row == col:
-                somme = 0.0
-                for diag in range(row):
-                    somme += L[row][diag] ** 2
-                L[row][col] = math.sqrt(A[row][col] - somme)
+                somme = np.sum(L[row, :row] ** 2)
+                L[row, col] = np.sqrt(A[row, col] - somme)
             else:
-                somme = 0.0
-                for diag in range(col):
-                    somme += L[row][diag] * L[col][diag]
-                L[row][col] = (A[row][col] - somme) / L[col][col]
+                somme = np.sum(L[row, :col] * L[col, :col])
+                L[row, col] = (A[row, col] - somme) / L[col, col]
     return L
 
-def descente(L: list[list[float]], b: list[list[float]]) -> list[list[float]]:
-    n = len(b)
-    z = [[0.0] for _ in range(n)]
+def descente(L: np.ndarray, b: np.ndarray) -> np.ndarray:
+    n = b.shape[0]
+    z = np.zeros((n, 1), dtype=np.float64)
 
     for i in range(n):
-        somme = 0.0
-        for j in range(i):
-            somme += L[i][j] * z[j][0]
-        z[i][0] = (b[i][0] - somme) / L[i][i]
+        somme = np.sum(L[i, :i] * z[:i, 0])
+        z[i] = (b[i] - somme) / L[i, i]
 
     return z
 
-
-def algorithme(C: list[list[float]], b: list[list[float]]):
+def algorithme(C: np.ndarray, b: np.ndarray, with_print: bool = False):
     A = produit_matrix_transpose(transpose(C), C)
     L = calcul_L(A)
     L_T = transpose(L)
     z = descente(L, b)
     x = remonte(L_T, z)
-    print_matrix(x)
-    print("\nRésidu\n")
-    print_matrix(residu(A, b, x))
+    if with_print:
+        print("\nMatrice A\n")
+        print_matrix(A)
+        print("\nMatrice L\n")
+        print_matrix(L)
+        print("\nMatrice L_T\n")
+        print_matrix(L_T)
+        print("\nMatrice z\n")
+        print_matrix(z)
+        print("\nMatrice x\n")
+        print_matrix(x)
+        print("\nRésidu\n")
+        print_matrix(residu(A, b, x))
+    return x
 
 if __name__ == "__main__":
-    algorithme(C, b)
+    algorithme(C, b, with_print=True)
--- S6/ALN/TP1/generate_matrix.py
+++ S6/ALN/TP1/generate_matrix.py
@@ -1,11 +1,12 @@
 import random
+import numpy as np
 from houselder import print_matrix
 
-def generate_matrix(n: int, m: int, min: float = -10, max: float = 10) -> list[list[float]]:
-    return [[random.uniform(min, max) for _ in range(m)] for _ in range(n)]
+def generate_matrix(n: int, m: int, min: float = -10, max: float = 10) -> np.ndarray:
+    return np.random.uniform(min, max, (n, m))
 
-def generate_vector(n: int, min: float = -10, max: float = 10) -> list[float]:
-    return [random.uniform(min, max) for _ in range(n)]
+def generate_vector(n: int, min: float = -10, max: float = 10) -> np.ndarray:
+    return np.random.uniform(min, max, n)
 
 if __name__ == "__main__":
     print("Matrix:\n")
