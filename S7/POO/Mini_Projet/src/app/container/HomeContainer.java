f04f2db5fe13a95eafda89ecd9fecd173548b7ec
Simon Desdevises <simon.desdevises@gmail.com>
2025-10-08 08:19:44 +0200

    add label for input

--- S7/POO/Mini_Projet/src/app/container/HomeContainer.java
+++ S7/POO/Mini_Projet/src/app/container/HomeContainer.java
@@ -17,12 +17,14 @@ public class HomeContainer extends JPanel {
 
   private JButton startButton;
   private JLabel resultLabel;
+  private JLabel gridSizeLabel;
   private JTextField gridSizeField;
+  private JLabel nbBombeLabel;
   private JTextField nbBombeField;
 
   public HomeContainer(Status status) {
     super();
-    this.setLayout(new GridLayout(status == Status.NEW ? 3 : 4, 1));
+    this.setLayout(new GridLayout(status == Status.NEW ? 5 : 6, 1));
 
     if (status != Status.NEW) {
       resultLabel = new JLabel(status == Status.WIN ? "Gagné !" : "Perdu !");
@@ -37,9 +39,17 @@ public class HomeContainer extends JPanel {
     this.add(this.startButton);
 
     this.gridSizeField = new JTextField("10");
+    this.gridSizeLabel = new JLabel("Taille de la grille :");
+    this.gridSizeLabel.setHorizontalAlignment(JLabel.CENTER);
+    this.gridSizeLabel.setVerticalAlignment(JLabel.CENTER);
+    this.add(this.gridSizeLabel);
     this.add(this.gridSizeField);
 
     this.nbBombeField = new JTextField("10");
+    this.nbBombeLabel = new JLabel("Nombre de bombes :");
+    this.nbBombeLabel.setHorizontalAlignment(JLabel.CENTER);
+    this.nbBombeLabel.setVerticalAlignment(JLabel.CENTER);
+    this.add(this.nbBombeLabel);
     this.add(this.nbBombeField);
 
     this.startButton.addActionListener(e -> onStartButtonClick());
