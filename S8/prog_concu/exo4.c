f6e1bf04fe3b38e369786ccf2bb1b5e0e4f819ee
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-03-02 16:36:23 +0100

    exo 4

---
+++ S8/prog_concu/exo4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int first_child, second_child;
+
+  first_child = fork();
+  if (first_child < 0) {
+    perror("Create first child");
+    exit(1);
+  }
+  if (first_child == 0) {
+    second_child = fork();
+    if (second_child < 0) {
+      perror("Create second child");
+      exit(1);
+    }
+    if (second_child == 0) {
+      sleep(4);
+      printf("C'est le second fils qui écrit, pid: %d , ppid: %d\n", getpid(),
+             getppid());
+      return 0;
+    }
+    sleep(2);
+    printf("C'est le premier fils qui écrit, pid: %d , ppid: %d\n", getpid(),
+           getppid());
+    waitpid(second_child, NULL, 0);
+    return 0;
+  }
+
+  printf("C'est le père qui écrit, pid: %d , ppid: %d\n", getpid(), getppid());
+  waitpid(first_child, NULL, 0);
+
+  return 0;
+}
