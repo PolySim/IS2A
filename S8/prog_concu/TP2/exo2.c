15e93d3125508325f5159b21c43ddd45dab0e1b6
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-03-03 14:01:40 +0100

    exo 2

---
+++ S8/prog_concu/TP2/exo2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  char fich[256];
+
+  printf("Entrez le nom du fichier : ");
+  scanf("%s", fich);
+
+  int pid = fork();
+  if (pid < 0) {
+    perror("fork");
+    exit(1);
+  }
+
+  if (pid == 0) {
+    execlp("wc", "wc", "-l", fich, NULL);
+    perror("execlp");
+    exit(1);
+  } else {
+    wait(NULL);
+  }
+
+  return 0;
+}
