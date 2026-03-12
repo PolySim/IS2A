324768d1b5b23159ab114af280967169f72b8c14
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-03-12 10:44:00 +0100

    Q1

--- S8/SIO/tpJDBC/src/is/sio/tpJDBC/AccesCompte.java
+++ S8/SIO/tpJDBC/src/is/sio/tpJDBC/AccesCompte.java
@@ -1,48 +1,54 @@
+package is.sio.tpJDBC;
 
-package is.sio.tpJDBC ;
-
-import java.io.FileInputStream ;
-import java.io.IOException ;
-import java.util.Properties ;
-import java.sql.* ;
-
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class AccesCompte {
-	
-  public static void main(String args[]) {
-    try {
-      String nomBase=args[0] ; String idClient=args[1] ;
-      Properties props=new Properties() ;
-      //lecture du fichier database.properties
-      FileInputStream in=new FileInputStream("database.properties") ; 
-      props.load(in) ; 
-      String server=props.getProperty("jdbc.server") ; 
-      String username=props.getProperty("jdbc.user") ; 
-      String password=props.getProperty("jdbc.password") ;    
-      String protocoleURL=props.getProperty("jdbc.protocole") ;
 
-      // constitution de l'URL à compléter
-      String url= "";
-      
-      // programme :
-      Compte compte = new Compte(url,username, password, idClient) ;
-      System.out.println("compte : "+compte.getIdClient()+
-                         ", solde : "+compte.getSolde()) ;
-      compte.setSolde(compte.getSolde()+20.0) ;
-      System.out.println("ajout de 20.0 au compte") ;
-      System.out.println("compte : "+compte.getIdClient()+
-                         ", solde : "+compte.getSolde()) ;
-      compte.close() ;
-    } catch(ArrayIndexOutOfBoundsException e0) {
-      System.err.println("erreur argument: java AccesCompte nomBase idClient") ;
-    } catch(IOException e1) {
-      System.err.println("erreur fichier database.properties") ;
-    } catch(SQLException e3) {
-      System.err.println("erreur SQL : "+e3.getMessage()) ;
-    } catch(CompteInconnuException e4) {
-      System.err.println("compte client inconnu") ;
+    public static void main(String args[]) {
+        try {
+            String nomBase = args[0];
+            String idClient = args[1];
+            Properties props = new Properties();
+            //lecture du fichier database.properties
+            FileInputStream in = new FileInputStream("database.properties");
+            props.load(in);
+            String server = props.getProperty("jdbc.server");
+            String username = props.getProperty("jdbc.user");
+            String password = props.getProperty("jdbc.password");
+            String protocoleURL = props.getProperty("jdbc.protocole");
+
+            String url = protocoleURL + "://" + server + "/" + nomBase;
+
+            // programme :
+            Compte compte = new Compte(url, username, password, idClient);
+            System.out.println(
+                "compte : " +
+                    compte.getIdClient() +
+                    ", solde : " +
+                    compte.getSolde()
+            );
+            compte.setSolde(compte.getSolde() + 20.0);
+            System.out.println("ajout de 20.0 au compte");
+            System.out.println(
+                "compte : " +
+                    compte.getIdClient() +
+                    ", solde : " +
+                    compte.getSolde()
+            );
+            compte.close();
+        } catch (ArrayIndexOutOfBoundsException e0) {
+            System.err.println(
+                "erreur argument: java AccesCompte nomBase idClient"
+            );
+        } catch (IOException e1) {
+            System.err.println("erreur fichier database.properties");
+        } catch (SQLException e3) {
+            System.err.println("erreur SQL : " + e3.getMessage());
+        } catch (CompteInconnuException e4) {
+            System.err.println("compte client inconnu");
+        }
     }
-  }
 }
-
