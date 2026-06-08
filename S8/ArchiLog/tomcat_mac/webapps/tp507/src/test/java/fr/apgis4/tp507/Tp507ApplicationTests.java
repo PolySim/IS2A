125f9b75bd6f861b3a8979367b1b9b2811811e00
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-06-08 10:27:24 +0200

    init TP 7

---
+++ S8/ArchiLog/tomcat_mac/webapps/tp507/src/test/java/fr/apgis4/tp507/Tp507ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apgis4.tp507;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp507ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
