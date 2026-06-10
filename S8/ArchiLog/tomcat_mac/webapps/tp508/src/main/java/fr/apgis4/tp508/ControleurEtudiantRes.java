4fd75e12c4884172ea332942833e5aa43410d001
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-06-10 16:49:32 +0200

    exo 2

--- S8/ArchiLog/tomcat_mac/webapps/tp508/src/main/java/fr/apgis4/tp508/ControleurEtudiantRes.java
+++ S8/ArchiLog/tomcat_mac/webapps/tp508/src/main/java/fr/apgis4/tp508/ControleurEtudiantRes.java
@@ -1,8 +1,13 @@
 package fr.apgis4.tp508;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
@@ -47,6 +52,72 @@ public class ControleurEtudiantRes {
     } else {
       Map<String, Object> error = new HashMap<>();
       error.put("message", "Aucun étudiant trouvé pour le groupe " + groupe);
+      error.put("status", 404);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+  }
+
+  @PostMapping("/etudiants")
+  public ResponseEntity<Object> ajouterEtudiant(@RequestBody Etudiant etudiant) {
+    Etudiant savedEtudiant = etudiantRepository.save(etudiant);
+    return ResponseEntity.ok(savedEtudiant);
+  }
+
+  @PutMapping("/etudiants/{id}")
+  public ResponseEntity<Object> modifierEtudiant(@PathVariable int id, @RequestBody Etudiant etudiant) {
+    Optional<Etudiant> existingEtudiant = etudiantRepository.findById(id);
+    if (existingEtudiant.isPresent()) {
+      Etudiant updatedEtudiant = existingEtudiant.get();
+      updatedEtudiant.setNom(etudiant.getNom());
+      updatedEtudiant.setPrenom(etudiant.getPrenom());
+      updatedEtudiant.setAge(etudiant.getAge());
+      updatedEtudiant.setGroupe(etudiant.getGroupe());
+      Etudiant savedEtudiant = etudiantRepository.save(updatedEtudiant);
+      return ResponseEntity.ok(savedEtudiant);
+    } else {
+      Map<String, Object> error = new HashMap<>();
+      error.put("message", "L'étudiant avec l'id " + id + " n'existe pas");
+      error.put("status", 404);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+  }
+
+  @PatchMapping("/etudiants/{id}")
+  public ResponseEntity<Object> modifierEtudiantPartiellement(@PathVariable int id, @RequestBody Etudiant etudiant) {
+    Optional<Etudiant> existingEtudiant = etudiantRepository.findById(id);
+    if (existingEtudiant.isPresent()) {
+      Etudiant updatedEtudiant = existingEtudiant.get();
+      if (etudiant.getNom() != null) {
+        updatedEtudiant.setNom(etudiant.getNom());
+      }
+      if (etudiant.getPrenom() != null) {
+        updatedEtudiant.setPrenom(etudiant.getPrenom());
+      }
+      if (etudiant.getAge() != null) {
+        updatedEtudiant.setAge(etudiant.getAge());
+      }
+      if (etudiant.getGroupe() != null) {
+        updatedEtudiant.setGroupe(etudiant.getGroupe());
+      }
+      Etudiant savedEtudiant = etudiantRepository.save(updatedEtudiant);
+      return ResponseEntity.ok(savedEtudiant);
+    } else {
+      Map<String, Object> error = new HashMap<>();
+      error.put("message", "L'étudiant avec l'id " + id + " n'existe pas");
+      error.put("status", 404);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+  }
+
+  @DeleteMapping("/etudiants/{id}")
+  public ResponseEntity<Object> supprimerEtudiant(@PathVariable int id) {
+    Optional<Etudiant> existingEtudiant = etudiantRepository.findById(id);
+    if (existingEtudiant.isPresent()) {
+      etudiantRepository.delete(existingEtudiant.get());
+      return ResponseEntity.ok().build();
+    } else {
+      Map<String, Object> error = new HashMap<>();
+      error.put("message", "L'étudiant avec l'id " + id + " n'existe pas");
       error.put("status", 404);
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
