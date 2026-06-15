5795849091cba10cf302bf16aec92af1139a946e
Simon Desdevises <sdesdevises@faire-savoir.com>
2026-06-15 11:45:06 +0200

    exo 3

--- S8/ArchiLog/tomcat_mac/webapps/tp509/src/main/java/fr/apgis4/tp509/MonController.java
+++ S8/ArchiLog/tomcat_mac/webapps/tp509/src/main/java/fr/apgis4/tp509/MonController.java
@@ -1,6 +1,9 @@
 package fr.apgis4.tp509;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MonController {
+
+private final JdbcTemplate jdbcTemplate;
+
+  public MonController(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
   @RequestMapping("/public")
   public String publicV1() {
@@ -26,5 +35,20 @@ public class MonController {
     String name = authentication != null ? authentication.getName() : null;
     model.addAttribute("name", name);
     return "private/v2";
+  }
+
+  @RequestMapping("/users")
+  public String users(Model model) {
+    List<UserRow> users = jdbcTemplate.query(
+        "select username, password, enabled from users order by username",
+        (rs, rowNum) -> new UserRow(
+            rs.getString("username"),
+            rs.getString("password"),
+            rs.getBoolean("enabled")));
+    model.addAttribute("users", users);
+    return "private/users";
+  }
+
+  public record UserRow(String username, String password, boolean enabled) {
   }
 }
--- S8/ArchiLog/tomcat_mac/webapps/tp509/src/main/webapp/WEB-INF/jsp/private/users.jsp
+++ S8/ArchiLog/tomcat_mac/webapps/tp509/src/main/webapp/WEB-INF/jsp/private/users.jsp
@@ -1,3 +1,21 @@
-<% for (User user : users) { %>
-  <li><%= user.getName() %></li>
-<% } %>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page import="java.util.List" %>
+<%@ page import="fr.apgis4.tp509.MonController.UserRow" %>
+<%
+  List<UserRow> users = (List<UserRow>) request.getAttribute("users");
+%>
+<h1>Table users</h1>
+<table border="1">
+  <tr>
+    <th>username</th>
+    <th>password</th>
+    <th>enabled</th>
+  </tr>
+  <% for (UserRow user : users) { %>
+  <tr>
+    <td><%= user.username() %></td>
+    <td><%= user.password() %></td>
+    <td><%= user.enabled() %></td>
+  </tr>
+  <% } %>
+</table>
